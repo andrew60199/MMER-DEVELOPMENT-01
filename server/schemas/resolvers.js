1735e6ff12122d46802d84cee2a91f4cc882c903
AND.EVV <andrew60199@gmail.com>
2023-02-05 12:00:00 +1100

    worked on defining the type definitions and their accompanying resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
+const { User, Client, Employee } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
@@ -11,6 +11,18 @@ const resolvers = {
         })
       }
       throw new AuthenticationError('You need to be logged in!');
+    },
+    client: async (parent, args, context) => {
+      if (context.user) {
+        return await Client.findOne({ userId: context.user._id })
+      }
+      throw new AuthenticationError('Unknown client');
+    },
+    employee: async (parent, args, context) => {
+      if (context.user) {
+        return await Employee.findOne({ userId: context.user._id })
+      }
+      throw new AuthenticationError('Unknown employee');
     },
   },
 
